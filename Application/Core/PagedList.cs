6bec119ef2a8e8c8d56fbb51979b54b2db56b00e
TWhela <thomas.whelan@ntlworld.com>
2024-01-12 16:17:26 +0000

    continued prepping api for pagination

--- Application/Core/PagedList.cs
+++ Application/Core/PagedList.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Core
 {
@@ -10,6 +10,7 @@ namespace Application.Core
             TotalCount = (int)Math.Ceiling(count / (double)pageSize);
             PageSize = pageSize;
             TotalCount = count;
+            AddRange(items);
         }
 
         public int CurrentPage { get; set; }
@@ -17,5 +18,11 @@ namespace Application.Core
         public int PageSize { get; set; }
         public int TotalCount { get; set; }
 
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync(); 
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
     }
 }
